c18042ffc0299eb3607ac90a9df19b9d0b551fe1
Robert Prus <robert.prus87@gmail.com>
2023-05-06 20:45:59 +0200

    work in progress

--- moutain_trips_meter/moutain_trip_meter_app.py
+++ moutain_trips_meter/moutain_trip_meter_app.py
@@ -4,5 +4,20 @@ LOCATIONS = {}
 
 with open('guide_locations.csv', 'r', encoding='UTF-8') as file:
     reader = csv.reader(file)
+    next(reader)
     for row in reader:
-        id, height, location_name, mountain_range = row[0], row[1], row[2], row[3]
+        id, height, location_name, mountain_range = int(row[0]), int(row[1]), row[2], int(row[3])
+        LOCATIONS.update({location_name: {'id': id, 'height': height, 'mountain_range': mountain_range}})
+# print(LOCATIONS)
+
+counter = 0
+
+while counter < 3:
+    user_input = input("Provide starting point: ")
+    if user_input:
+        location = LOCATIONS.get(user_input, "Location has not been found")
+        print(location)
+    else:
+        print("Closing ...")
+        exit(1)
+    counter += 1
