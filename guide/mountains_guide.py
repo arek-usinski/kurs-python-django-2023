44e211f439f9f1ef35a70776f2eeda6d572fe355
Joanna Paterek <j.krawczyk87@gmail.com>
2023-05-06 20:33:04 +0200

    feat: create dictionary with locations and get user input

--- guide/mountains_guide.py
+++ guide/mountains_guide.py
@@ -6,5 +6,19 @@ with open('guide_locations.csv', 'r', encoding='UTF-8') as file:
     reader = csv.reader(file)
     next(reader)
     for row in reader:
-        id, height, location_name, mountain_range = map(str.strip, row)
-        print(id, height, location_name, mountain_range)
+        id, height, location_name, mountain_range = int(row[0]), int(row[1]), row[2], int(row[3])
+        LOCATIONS.update({location_name: {'id': id, 'height': height, 'mountain_range': mountain_range}})
+
+flag = True
+
+while flag:
+    user_input = input("Podaj punkt startowy podróży: ")
+    if user_input and user_input in LOCATIONS:
+        location = LOCATIONS.get(user_input, "Location not found. Try again!")
+        # id = LOCATIONS[user_input]['id']
+        print(location)
+        flag = False
+    else:
+        print("Try again. Location not found!")
+
+# print(LOCATIONS)
