45df928d0e132d7230a87f1ae1bd9f3277297253
Joanna Paterek <j.krawczyk87@gmail.com>
2023-05-06 20:46:27 +0200

    feat: handle user input

--- guide/mountains_guide.py
+++ guide/mountains_guide.py
@@ -10,15 +10,20 @@ with open('guide_locations.csv', 'r', encoding='UTF-8') as file:
         LOCATIONS.update({location_name: {'id': id, 'height': height, 'mountain_range': mountain_range}})
 
 flag = True
+counter = 0
 
-while flag:
+while flag and counter < 3:
     user_input = input("Podaj punkt startowy podróży: ")
-    if user_input and user_input in LOCATIONS:
+    if not user_input:
+        print("No input. Try again!")
+        counter += 1
+    elif user_input and user_input in LOCATIONS:
         location = LOCATIONS.get(user_input, "Location not found. Try again!")
         # id = LOCATIONS[user_input]['id']
-        print(location)
+        print("Your choice:", user_input, location)
         flag = False
     else:
         print("Try again. Location not found!")
+        counter += 1
 
 # print(LOCATIONS)
