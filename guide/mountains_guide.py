0e88b9dfe6cc33f5dc23c1be6fdb37528aa12261
Joanna Paterek <j.krawczyk87@gmail.com>
2023-05-06 19:51:46 +0200

    feat: skip first line of csv file (headings)

--- guide/mountains_guide.py
+++ guide/mountains_guide.py
@@ -4,6 +4,7 @@ LOCATIONS = {}
 
 with open('guide_locations.csv', 'r', encoding='UTF-8') as file:
     reader = csv.reader(file)
+    next(reader)
     for row in reader:
         id, height, location_name, mountain_range = map(str.strip, row)
-        print(id, location_name)
+        print(id, height, location_name, mountain_range)
